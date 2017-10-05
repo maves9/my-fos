52fd8b6ab987af5c658836a38fc9485bc30591fd
Martin vestergaard <mvmartinxd@gmail.com>
2017-10-05 14:21:34 +0200

    Add files via upload

---
+++ zip/js/checkBoxes.js
@@ -0,0 +1,21 @@
+$('#allFiles').change(function(){
+	if ($('#allFiles').is(':checked')) {
+    	$("#fileBrowser li input").prop('checked', true);
+	}
+	else {
+		$("#fileBrowser li input").prop('checked', false);
+	}
+});
+$('.folder input').change(function(){
+	if ($(this).is(':checked')) {
+    	$(this).parent('li').find('ul li input').prop('checked', true);
+
+	}
+	else {
+		$(this).parent('li').find('ul li input').prop('checked', false);
+	}
+});
+
+$('.openClose').click(function(){
+	$(this).parent('li').find('ul li').toggle();
+});
